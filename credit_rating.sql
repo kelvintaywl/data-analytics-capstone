80f9ca7c1d7ec6ea2743998653c728aee079a66a
kelvintaywl <kelvintaywl@gmail.com>
2023-12-03 17:32:04 +0900

    docs: add comment on rating field

--- credit_rating.sql
+++ credit_rating.sql
@@ -10,9 +10,10 @@ SELECT
   ID,
   HISTORY,
   (CASE
+    /* any 5 in history means this customer is risky */
     WHEN HISTORY LIKE "%5%" THEN -1
+    /* only 0, C or X, so this customer is good */
     WHEN HISTORY REGEXP "\b(C|X|0)+\b" THEN 1
     ELSE 0 END
   )	AS RATING
 FROM credit_history;
-
